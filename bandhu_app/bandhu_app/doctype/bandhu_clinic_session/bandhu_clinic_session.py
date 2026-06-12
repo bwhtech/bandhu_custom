a8431058a395ba61758a8ba4b070f569aa1ad509
Aditya <adypatanwar07@gmail.com>
2026-06-12 16:41:46 +0000

    feat: implement clinic session, location, and patient data enhancements

--- bandhu_app/bandhu_app/doctype/bandhu_clinic_session/bandhu_clinic_session.py
+++ bandhu_app/bandhu_app/doctype/bandhu_clinic_session/bandhu_clinic_session.py
@@ -14,14 +14,14 @@ class BandhuClinicSession(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
-		arrival_time: DF.Datetime | None
+		arrival_time: DF.Time | None
 		assigned_doctor: DF.Link | None
 		assigned_driver: DF.Link | None
 		assigned_nurse: DF.Link | None
 		clinic: DF.Link
 		date: DF.Date
-		departure_time: DF.Datetime | None
-		distance_travelled: DF.Data | None
+		departure_time: DF.Time | None
+		distance_travelled_km: DF.Data | None
 		end_time: DF.Datetime | None
 		notes: DF.SmallText | None
 		project: DF.Link
